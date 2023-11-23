753d1131afe77182048a37d7bf0d1e587a93a4b9
Bruno Santos Silva <jkbrunowk@hotmail.com>
2023-11-22 22:14:42 -0300

    efeito de css aplicado na homepage, e data.js criada

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let loading = document.querySelector('.loading');
 let scrollPosition = 0;
-let count = 1;
+
 
 
 
@@ -29,4 +29,12 @@ function upDateLoading() {
     
 
     loading.style.width = `${percentage}%`;
+}
+
+divertissementUpdate()
+function divertissementUpdate() {
+    let divContainer = document.querySelector('.container_informations');
+    let numberImages= data.divertissement;
+    console.log(numberImages)
+    divContainer.innerHTML = '<div class="informations_box"><div class="image_informations"><img src="./images/divertissement/playgroundInfantil.jpeg" alt=""></div><div class="text_box_informations"><h1>Playground Infantil</h1><p>Playground no gramado para crianças</p><div class="line_information"></div></div></div>'
 }
---
+++ src/data.js
@@ -0,0 +1,12 @@
+let divertissement = [
+    {title: 'Playground Infantil', subtitle:'Playground no gramado para as crianças',img:''},
+    {title: 'Salão de Jogos', subtitle:'Com mesas de bilhar, sinuca , ping-pong, pebolim e air hockey.',img:''},
+    {title: 'Campo de Futebol e Volei', subtitle:'Um campo gramado de futebol e um campo gramado de volei',img:''},
+    {title: 'Salão de Festas', subtitle:'Salão fechado para festas e eventos com capacidade para 180 pessoas sentadas',img:''},
+    {title: 'Piscinas Infantil', subtitle:'Piscina para crianças com profundidade crescente entre 20cm e 60cm',img:''},
+    {title: 'Salão Cerimonial', subtitle:'Salão fechado para realização de casamentos, cultos e apresentações',img:''},
+    {title: 'Alimentação', subtitle:'Possibilidade de contratar nosso buffet',img:''},
+    {title: 'Fogueira de Inverno', subtitle:'Fogueira de inverno com banco circular ',img:''},
+    {title: 'Casinha de Boneca', subtitle:'Casinha de boneca para visitação',img:''},
+    {title: 'Lago', subtitle:'Lago com possibilidade limitada de pesca para grupo pequeno ',img:''}
+]
